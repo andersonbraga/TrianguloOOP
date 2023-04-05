f8da67f03379ebd33c714b96d5f5b27cc41ef44b
Anderson <anderson.braga@uniplaclages.edu.br>
2023-04-05 17:24:22 -0300

    Add project files.

---
+++ TrianguloOOP.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TrianguloOOP.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+            triangulo.x = 5; triangulo.y = 5; triangulo.z = 5;
+            Console.WriteLine(triangulo.VerificarTipoTriangulo());
+        }
+    }
+}
---
+++ TrianguloOOP.ConsoleApp/Triangulo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloOOP.ConsoleApp
+{
+    internal class Triangulo
+    {
+        public double x;
+        public double y;
+        public double z;
+        public string tipoTriangulo =" ";
+
+        public string VerificarTipoTriangulo()
+        {
+            if (x < y + z && y < x + z && z < x + y)
+            {
+                if (x == y && x == z && y == x && y == z && z == x && z == y)
+                {
+                     bool equilatero = true;
+                     return tipoTriangulo = "equilatero";
+                }
+                else if (x == y && x != z || x == z && x != y || y == x && y != z || y == z && y != x
+                         || z == x && z != y || z == y && z != x)
+                {
+                     bool isosceles = true;
+                     return tipoTriangulo = "isosceles";
+                } 
+                bool escaleno = true;
+               return tipoTriangulo = "escaleno";
+                
+            }
+           
+            bool triangulo = false;
+            return tipoTriangulo = "Não é um triangulo";
+        }
+
+
+    }
+}
